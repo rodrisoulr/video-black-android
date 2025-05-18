a35bb3db009732366bb017a57c1df2e1b2224d2f
rodrisoulr <rodrigo.a@soulrefiner.com>
2025-05-18 20:36:36 -0300

    refreshing video src

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ export default function App() {
     <View style={styles.container}>
       <StatusBar style="auto" />
       <VideoPlayer 
-        source="https://5gsht45h4h24tgfjy-mobile.ext.bibly.com/community/comun_ruegrccT6WcZAUHZ97UM1234/video/7/index.m3u8?Expires=1747519640&Key-Pair-Id=K23COT2V2EOJI0&Signature=m88m9GWFbZ3Sly2LpSfb-inVS-9DZOIyu0~xonTZHx1fjog94ZCPhzmzXP7hj4X3mBTc8r3ICaRu~XyES84QP15r23UPD6Eju6G8CeiO-M9QVM6Dvmf4NOSwQ5wcAO7rZ8jGSVwqIOlB12oP--1DxbDSiJFpXaX2bd6rj5tGPjofxetdCDmX~r4M0SqJxg~F-yAQxJPsN5XF-FMg2NDiwNOn02IhvbPFIcDasj-ku4YKCZWZLOanC5i02QjyJLvm1bMCPIq~rineFr5PKpjGUOy21Q-c547r6LgFXHz86m2bfUGDNy4sKUg4iTRzs0MfnEMmogpeKts0BK0tIPvH1w__"
+        source="https://5gsht45h4h24tgfjy-mobile.ext.bibly.com/community/comun_ruegrccT6WcZAUHZ97UM1234/video/6/index.m3u8?Expires=1747697748&Key-Pair-Id=K23COT2V2EOJI0&Signature=eqZQTwXgfSCUYBCldZ~FaOSuvcnbhhHRBvyw2yJAuh4arf2AKbME3MAmseDGO7y8Efp5OEdW9nV4jvT932Q6g6SdGNpaptBJJJ7kzvUrQnP~CWf2RExoGZp-jMHcYKvw9IIhjatSJE2GByBYuAY0vFGVO5MryS-yF6IwMJn~6okFfX6U6SYuSq1T8~pqwl0Nv6e~t6APJL7YnnIrmKomCmqkQ8VUQk7-QmOY0N0eGuFOLHSi4Mkrk~QprDqk0va0tcGp8nI2pN2nnGO3XlOW5KxPXNLIbs4k6ALUKMdjE2wSvhuXYnrq-~LtwP5wQqNkQlxujlVBOCM3HV7nlthxeg__"
       />
     </View>
   );
