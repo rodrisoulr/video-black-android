b34f782dd372743fa7a0336f8d0cc357f8885e78
rodrisoulr <rodrigo.a@soulrefiner.com>
2025-06-09 11:12:35 -0300

    updating src and packages

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ export default function App() {
     <View style={styles.container}>
       <StatusBar style="auto" />
       <VideoPlayer 
-        source="https://5gsht45h4h24tgfjy-mobile.ext.bibly.com/community/comun_ruegrccT6WcZAUHZ97UM1234/video/7/index.m3u8?Expires=1747862623&Key-Pair-Id=K23COT2V2EOJI0&Signature=M66uOrdL4RG7IoPiHsTOKfg0o8CJhntfac7kl-JJWz9wZj5r3H1pbnOyLCIU8yhMLUuKBRSBa5QhjqADxXhXO09~jrOLhaLJZ-~K-1M70VD5HpRQCqDMt7AbwB~4mPE1mM5hiBpIcLBLePG7wYXcG8Rl-VGSSkKv8M2Z~JOAh8BMwh2bDSkxOsYrEFrXXtrASWVU-mkii~WieICCjoEZwA5yNssymOga2Dsoo8M82fQm1I~1FibVQpY65LXj6WwYNQGqWMkcIuBUyN8q2A7ChHPqATaoLIBxDWFaWcJ4X47Xf-gu~qKXX5jxR1fx1XPCADsLarzbaN7vMVEbTh-yuQ__"
+        source="https://5gsht45h4h24tgfjy-mobile.ext.bibly.com/community/comun_ruegrccT6WcZAUHZ97UM1234/video/4/index.m3u8?Expires=1749564499&Key-Pair-Id=K23COT2V2EOJI0&Signature=GIuegPYGKN9BnpaUdxTqmLeHsi90AAI0fY0qJmqYVO045OFjZsNDvsgr4Brh7XJqae~3lO~V0AuljRn1Bb~DnVu8zIeBQ7t3XR38iG-a1aDTOfxOjEuLIl-ObO5NksSqJbSBVgd6TUSII54iGh5kHa~U3o8cW1NhqvbYeCuL8ROeIYposHkkD9R7PJO39X00dWaxA4buqIuRL30fj5M6rngCSDX4uCcS1-95TIjSexUW6oYxEMRQuF7YPTeZ047rUyYSDD5v6IuZLdAonmBBjEwF0ABDpMDe6J0~WL7DL~0jgKdcFOK94P2oaLHXQx4dd0PBwJ-CG-HgYeUnZWVdNg__"
       />
     </View>
   );
