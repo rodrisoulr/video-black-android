1707775efcc8957a5902de03906dffeb35b15083
rodrisoulr <rodrigo.a@soulrefiner.com>
2025-06-15 21:45:34 -0300

    updating src

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { VideoPlayer } from './video-player';
 import { VideoSource } from 'expo-video';
 
 const videoSource: VideoSource = {
-  uri: 'https://5gsht45h4h24tgfjy-mobile.ext.bibly.com/community/comun_ruegrccT6WcZAUHZ97UM1234/video/7/index.m3u8?Expires=1749924748&Key-Pair-Id=K23COT2V2EOJI0&Signature=Wau4ayOMAu8oj2jcTcSwzLaX4D1odGdB-yuRhNSvtPJfcYTUIxeqH~MaTihNFsUkoak8bdNG0Y7AX315qzVi78N-RVUYSyNSOS~Q6RAd50zJ0DyTFFHVqzOQHgKX9qu0c-3h2Y7ogMyBRDPp8tZJgj6se72O5o1uni6AogqxJK70N8e98V-nb~pW9fvj3UMTEkCTLpGndXlmEsHaCkVhYKtvuD1jnMsVx4CoZvkir2hc0UZGUdpz88hvN~rL07B-~WZc0SKnI8bqAOVkrtF1jyUSWpJPNzxcKdTlKvfpcONo3SNpiyliocI5~UUGCBqw~mS5ZE4IkdNeM0qo43eOlw__',
+  uri: 'https://5gsht45h4h24tgfjy-mobile.ext.bibly.com/community/comun_ruegrccT6WcZAUHZ97UM1234/video/4/index.m3u8?Expires=1750121098&Key-Pair-Id=K23COT2V2EOJI0&Signature=0rdYPMHIM24Yxw9HdRINyqE8eAthoxElVCHuFS371abC9dwiwPV5pb6-jfpvviWJkuyUb~GHAx5epnY7u0MfLSCub-yJ1gp5Ee2L~xCHmV9E6-nEhxJWMGIXlO1NQs2NjAQyNd1-~AY1G~TIoJpQOi3tPWOqm2FxBYUmSYTZgOFz4G7Y4QQAGAFn1NfLKae5~cz2PZoV2usuVILE7LT9W8Z4l-zcgwVsGPP4zqG~6DBcPqin8es67axBjm0d8~Dm-hkG82UHzP~aH5OvIDYgJzXWsy00ij6PMr~-pQa339S0XXQTYSGqBiJbZOzQWVs9Slpzjv9myIq~2R0h7f2KiQ__',
   contentType: 'hls',
 };
 
